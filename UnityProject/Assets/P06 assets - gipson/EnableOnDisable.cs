dfa47da6750b10199a3213409c6cd4eb8e2d7ccc
LivingValkyrie <deadwynn@gmail.com>
2015-11-06 13:57:05 -0600

    fixed client player spawning
    Signed-off-by: LivingValkyrie <deadwynn@gmail.com>

---
+++ UnityProject/Assets/P06 assets - gipson/EnableOnDisable.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+/// <summary>
+/// Author: Matt Gipson
+/// Contact: Deadwynn@gmail.com
+/// Domain: www.livingvalkyrie.com
+/// 
+/// Description: EnableOnDisable 
+/// </summary>
+public class EnableOnDisable : NetworkBehaviour {
+    #region Fields
+
+    #endregion
+
+    void Start() {
+        transform.SetParent(null);
+    }
+
+    void Update() { }
+
+    void OnDisable() {
+        //this.gameObject.SetActive(true);
+    }
+
+}
